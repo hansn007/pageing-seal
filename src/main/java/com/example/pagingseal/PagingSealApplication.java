9da28f76abbde8026dacf89c51000b1b2aea86ab
huluwa <huluwa@huluwadeMac-mini.local>
2019-05-30 16:07:23 +0800

    chs

---
+++ src/main/java/com/example/pagingseal/PagingSealApplication.java
@@ -0,0 +1,13 @@
+package com.example.pagingseal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagingSealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagingSealApplication.class, args);
+	}
+
+}
